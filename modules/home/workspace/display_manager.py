e399101585ab8c4dabac0d190b3ec446ea6b0268
umokee <hituaev@gmail.com>
2026-05-18 01:54:51 +1000

    10

--- modules/home/workspace/display_manager.py
+++ modules/home/workspace/display_manager.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from shared.lib import add_packages, enable_units, system_file
+from shared.lib import add_packages, system_file, systemd_unit
 
 
 def apply(conf: dict, helpers) -> None:
@@ -9,12 +9,38 @@ def apply(conf: dict, helpers) -> None:
 
     add_packages("ly")
 
-    enable_units("ly.service")
-
     system_file(
         "/etc/ly/config.ini",
         _ly_config(),
     )
+
+    systemd_unit(
+        "ly.service",
+        _ly_service(),
+    )
+
+
+def _ly_service() -> str:
+    return """
+[Unit]
+Description=Ly display manager
+Documentation=man:ly(1)
+After=systemd-user-sessions.service getty@tty2.service plymouth-quit-wait.service
+After=rc-local.service
+
+Conflicts=getty@tty2.service
+Conflicts=display-manager.service
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/ly
+Restart=always
+RestartSec=1
+
+[Install]
+Alias=display-manager.service
+WantedBy=graphical.target
+"""
 
 
 def _ly_config() -> str:
